daef72d49cf813d2ae5cd4114a88cf5e522f2653
MaXX <xiangxiang.ma@studio.unibo.it>
2016-08-03 16:18:07 +0200

    collision system alpha

--- Atlas/src/atlas/model/algorithms/AlgorithmBernesHut.java
+++ Atlas/src/atlas/model/algorithms/AlgorithmBernesHut.java
@@ -8,7 +8,10 @@ import atlas.model.Body;
 public class AlgorithmBernesHut implements Algorithm, java.io.Serializable {
 
 	private static final long serialVersionUID = -8563165727832267173L;
-
+	
+	
+	
+	
 	@Override
     public ArrayList<Body> exceuteUpdate(ArrayList<Body> bodies, double sec) {
         return null;
