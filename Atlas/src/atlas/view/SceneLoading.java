238e55d4242349c6177df5385c35907397f94671
MaXX <xiangxiang.ma@studio.unibo.it>
2016-08-12 00:03:13 +0200

    Loading screen while starting the simulation

--- Atlas/src/atlas/view/SceneLoading.java
+++ Atlas/src/atlas/view/SceneLoading.java
@@ -1,20 +1,32 @@
 package atlas.view;
 
-import javafx.scene.Group;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class SceneLoading extends Scene{
 	
-	protected static ImageView logo = new ImageView(new Image("/images/" + "logo.png"));
+	protected final static ImageView LOGO = new ImageView(new Image("/images/" + "logo.png"));
+	protected final static String BACKGROUND = "/images/" + "star.png";
+	final static int FONT_SIZE = 16;
 	
-	private Group root = new Group();
+	private BorderPane root = new BorderPane();
 	
 	public SceneLoading() {
 		super(new Pane());
-		root.getChildren().add(logo);
+		root.setStyle("-fx-background-color: black;");
+		root.setCenter(LOGO);
+		Label loadingText = new Label("Loading...");
+		loadingText.setTextFill(Color.WHITESMOKE);
+		loadingText.setFont(Font.font(FONT_SIZE));
+		root.setBottom(loadingText);
+		BorderPane.setAlignment(loadingText, Pos.CENTER);
 		this.setRoot(root);
 	}
 	
