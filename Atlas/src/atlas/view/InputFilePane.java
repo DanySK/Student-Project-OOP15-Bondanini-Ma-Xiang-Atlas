4c5ce9117deda7f6fcc8ffcc889ccbcb2ff466fa
MaXX <xiangxiang.ma@studio.unibo.it>
2016-09-17 13:18:07 +0200

    New load panel + two body bug fix

--- Atlas/src/atlas/view/InputFilePane.java
+++ Atlas/src/atlas/view/InputFilePane.java
@@ -5,45 +5,61 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.layout.VBox;
 
 /**
- * Generic file tab pane, files are organized in different sections, separated by tabs.
+ * Generic file tab pane, files are organized in different sections, separated
+ * by tabs.
+ * 
  * @author maxx
  *
  */
-public class InputFilePane extends TabPane{
-	
+public class InputFilePane extends TabPane {
+
 	private Optional<String> selectedPath = Optional.empty();
-//	private Map<String, List<File>> files = new HashMap<>();
-	
+
 	/**
-	 * Creates a tab pane using a map, each tab represents a folder and its the content the files
-	 * @param path the root path from which to search the files
+	 * Creates a tab pane using a map, each tab represents a folder and its the
+	 * content the files
+	 * 
+	 * @param path
+	 *            the root path from which to search the files
 	 */
 	public InputFilePane(Map<File, List<File>> files) {
-	    
-        //creates the tabs and buttons
+
+		// creates the tabs and list view
 		files.entrySet().forEach(i -> {
 			Tab tab = new Tab(i.getKey().getName());
-			VBox content = new VBox();
+
+			ListView<String> list = new ListView<>();
+
+			// Add each file to the list view
 			i.getValue().forEach(j -> {
-				Button btn = new Button(j.getName());
-				btn.setOnAction(e -> {
+
+				list.getItems().add(j.getName());
+				list.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+				
+				//upates the selected item
+				list.getSelectionModel().selectedItemProperty().addListener(l -> {
 					selectedPath = Optional.ofNullable(j.getAbsolutePath());
-					//Create lateral pane
 				});
-				content.getChildren().add(btn);
 			});
-			tab.setContent(content);
+
+			tab.setContent(new ScrollPane(list));
 			this.getTabs().add(tab);
 		});
 		this.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
 	}
-	
+
+	/**
+	 * Gets an optional of the selected item.
+	 * 
+	 * @return an optional the selected item
+	 */
 	public Optional<String> getSelectedPath() {
 		return selectedPath;
 	}
