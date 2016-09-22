37a63d8d7c986678640522b1a107f3d9c0a15c4f
MaXX <xiangxiang.ma@studio.unibo.it>
2016-09-23 00:24:33 +0200

    Refactoring view part 1

--- Atlas/src/atlas/view/CruiseControl.java
+++ Atlas/src/atlas/view/CruiseControl.java
@@ -1,5 +1,6 @@
 package atlas.view;
 
+import atlas.utils.Pair;
 import atlas.utils.Units;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -15,25 +16,25 @@ import javafx.scene.layout.HBox;
  * This class contains part of the UI which is used to control the simulation.
  * It provides the following functionalities: - Play/Pause - Show date or time
  * of the simulation - A way to change the simulation speed - EDIT and ADD
- * buttons - Search and eventually info/screenshots
+ * buttons - Center and deselect.
  * 
  * @author MaXX
  *
  */
 public class CruiseControl extends BorderPane {
-	protected Button buttonPlay = new Button("PLAY");// setGraphic
-	protected Button buttonStop = new Button("STOP");
-	protected Button buttonSpeed = new Button("SPEED");
-	protected Button buttonEdit = new Button("EDIT");
-	protected Button buttonAdd = new Button("ADD");
-	protected Button buttonCenter = new Button("CENTER");
-	protected Button buttonESC = new Button("DESELECT");
+	private Button buttonPlay = new Button("PLAY");// setGraphic
+	private Button buttonStop = new Button("STOP");
+	private Button buttonSpeed = new Button("SPEED");
+	private Button buttonEdit = new Button("EDIT");
+	private Button buttonAdd = new Button("ADD");
+	private Button buttonCenter = new Button("CENTER");
+	private Button buttonESC = new Button("DESELECT");
 	private boolean play;
 
-	protected Label labelTime = new Label("Sample: 01/01/2000");
+	private Label labelTime = new Label("Sample: 01/01/2000");
 
-	protected TextField textSpeed = new TextField();
-	protected ChoiceBox<Units> choiceSpeedUnit = new ChoiceBox<>();
+	private TextField textSpeed = new TextField();
+	private ChoiceBox<Units> choiceSpeedUnit = new ChoiceBox<>();
 
 	protected ViewMenuOption viewMenu = new ViewMenuOption();
 	private HBox left = new HBox();
@@ -42,9 +43,12 @@ public class CruiseControl extends BorderPane {
 
 	private View view;
 
+	/**
+	 * Constructor of the class.
+	 */
 	public CruiseControl() {
 		this.setId("cruise");
-	
+
 		this.view = ViewImpl.getView();
 		left.getChildren().add(0, buttonStop);
 		left.getChildren().add(1, labelTime);
@@ -56,12 +60,13 @@ public class CruiseControl extends BorderPane {
 		center.getChildren().add(buttonEdit);
 		center.getChildren().add(buttonAdd);
 		center.setAlignment(Pos.CENTER);
-		
+
 		right.setAlignment(Pos.CENTER);
 		right.getChildren().add(viewMenu);
-		right.getChildren().add(buttonCenter);;
+		right.getChildren().add(buttonCenter);
+		;
 		right.getChildren().add(buttonESC);
-		
+
 		this.setLeft(left);
 		this.setCenter(center);
 		this.setRight(right);
@@ -71,41 +76,67 @@ public class CruiseControl extends BorderPane {
 		/* Setting actions */
 		this.setActions();
 	}
-	
+
+	/**
+	 * Sets/updates the time label.
+	 * 
+	 * @param time
+	 *            the time of the simulation
+	 */
+	public void setTime(String time) {
+		this.labelTime.setText(time);
+	}
+
+	/**
+	 * Returns the text in the text field and the selected unit in the choice
+	 * box.
+	 * 
+	 * @return speed text and unit.
+	 */
+	public Pair<String, Units> getSpeed() {
+		return new Pair<>(this.textSpeed.getText(), this.choiceSpeedUnit.getValue());
+	}
+
+	/**
+	 * Determines which button to show and hide.
+	 */
 	private void switchPlayStop() {
 		left.getChildren().remove(this.play ? this.buttonPlay : this.buttonStop);
 		left.getChildren().add(0, this.play ? this.buttonStop : this.buttonPlay);
 		this.play = !this.play;
 	}
-	
-	/*Sets node actions*/
+
+	/**
+	 * Sets the components actions.
+	 */
 	private void setActions() {
-	    EventHandler<ActionEvent> stopPlayHandler = new EventHandler<ActionEvent>() {
-	        @Override
-	        public void handle(ActionEvent e) {
-	            switchPlayStop();
-	            if (e.getSource().equals(buttonPlay) || e.getSource().equals(buttonStop)) {
-	                CruiseControl.this.view.notifyObservers(SimEvent.SPACEBAR_PRESSED);
-	            } else {
-	                new IllegalAccessException("Button unknow(not play nor stop)");
-	            }
-	        }
-	    };
-	    
-	    this.buttonPlay.setOnAction(stopPlayHandler);
-	    this.buttonStop.setOnAction(stopPlayHandler);
-	    
-	    this.buttonAdd.setOnAction(e -> ViewImpl.getView().notifyObservers(SimEvent.ADD) );
-	    
-	    this.buttonCenter.setOnAction(e -> ViewImpl.getView().notifyObservers(SimEvent.CENTER));
-	    
-	    this.buttonSpeed.setOnAction(e -> {
-	        view.notifyObservers(SimEvent.SPEED_CHANGED);
-	    });
-	    
-	    this.buttonESC.setOnAction(e -> {
-	        ViewImpl.getView().setSelectedBody(null);
-	        this.view.notifyObservers(SimEvent.ESC);
-	    });
+		EventHandler<ActionEvent> stopPlayHandler = new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				switchPlayStop();
+				if (e.getSource().equals(buttonPlay) || e.getSource().equals(buttonStop)) {
+					CruiseControl.this.view.notifyObservers(SimEvent.SPACEBAR_PRESSED);
+				} else {
+					new IllegalAccessException("Button unknow(not play nor stop)");
+				}
+			}
+		};
+
+		this.buttonPlay.setOnAction(stopPlayHandler);
+		this.buttonStop.setOnAction(stopPlayHandler);
+
+		this.buttonAdd.setOnAction(e -> ViewImpl.getView().notifyObservers(SimEvent.ADD));
+
+		this.buttonCenter.setOnAction(e -> ViewImpl.getView().notifyObservers(SimEvent.CENTER));
+
+		this.buttonSpeed.setOnAction(e -> {
+			view.notifyObservers(SimEvent.SPEED_CHANGED);
+			this.textSpeed.setText("");
+		});
+
+		this.buttonESC.setOnAction(e -> {
+			ViewImpl.getView().setSelectedBody(null);
+			this.view.notifyObservers(SimEvent.ESC);
+		});
 	}
 }
--- Atlas/src/atlas/view/MenuHidable.java
+++ Atlas/src/atlas/view/MenuHidable.java
@@ -3,37 +3,50 @@ package atlas.view;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 
-/*
+/**
+ * This abstract class offers a hide button that when pressed it hides/shows the
+ * content.
  * 
+ * @author MaXX
+ *
  */
 public abstract class MenuHidable extends BorderPane {
-	
-	protected static final double TOP_WIDTH = 300;
-	protected static final double TOP_HEIGHT = 200;
-	
-	protected Button btn;	
-	
+
+	protected Button btn;
+
+	/**
+	 * 
+	 */
 	public MenuHidable() {
-		/*Set all children's font in the css file*/
+		/* Set all children's style in the css file */
 		this.setId("menu-hidable");
-		
+
 		btn = new Button("|||");
 		btn.setMaxHeight(Double.MAX_VALUE);
 		btn.setOnAction(e -> {
-			if(isShown()) {
+			if (isShown()) {
 				hideContent();
 			} else {
 				showContent();
 			}
 		});
 	}
-	
+
+	/**
+	 * Show the content of this container.
+	 */
 	public abstract void showContent();
-	
+
+	/**
+	 * Hides the content of this container by removing all its children. 
+	 */
 	public void hideContent() {
 		this.getChildren().removeIf(i -> !i.equals(btn));
 	}
-	
+
+	/**
+	 * @return whether the content is shown or not.
+	 */
 	public boolean isShown() {
 		return this.getChildren().size() > 1 && this.getChildren().contains(btn);
 	}
