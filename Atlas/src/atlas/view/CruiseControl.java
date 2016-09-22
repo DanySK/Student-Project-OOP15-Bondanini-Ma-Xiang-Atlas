a494f0c3701dda02210f5f6ec7778083597f6c35
MaXX <xiangxiang.ma@studio.unibo.it>
2016-09-22 18:13:56 +0200

    CSS refining

--- Atlas/src/atlas/view/CruiseControl.java
+++ Atlas/src/atlas/view/CruiseControl.java
@@ -44,7 +44,7 @@ public class CruiseControl extends BorderPane {
 
 	public CruiseControl() {
 		this.setId("cruise");
-		
+	
 		this.view = ViewImpl.getView();
 		left.getChildren().add(0, buttonStop);
 		left.getChildren().add(1, labelTime);
